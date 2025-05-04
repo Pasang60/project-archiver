9cb10f4d262495a127579d85fef712a640587fc0
Pasang60 <pasanggelbu@gmail.com>
2025-05-04 13:26:41 +0545

    exception handled in the project

--- projectarchiver/src/main/java/com/pasang/projectarchiver/auth/controller/AuthController.java
+++ projectarchiver/src/main/java/com/pasang/projectarchiver/auth/controller/AuthController.java
@@ -4,10 +4,12 @@
 package com.pasang.projectarchiver.auth.controller;
 
 import com.pasang.projectarchiver.auth.dto.request.AuthRequest;
+import com.pasang.projectarchiver.auth.message.AuthSwaggerDocumentationMessage;
 import com.pasang.projectarchiver.auth.service.AuthService;
 import com.pasang.projectarchiver.constant.SystemMessage;
 import com.pasang.projectarchiver.global.BaseController;
 import com.pasang.projectarchiver.global.GlobalApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController extends BaseController {
     private final AuthService authService;
 
+
+    @Operation(
+            summary = AuthSwaggerDocumentationMessage.CREATE_LOGIN_SUMMARY,
+            description = AuthSwaggerDocumentationMessage.CREATE_LOGIN_DESCRIPTION
+    )
     @PostMapping("/login")
     public ResponseEntity<GlobalApiResponse> login(@RequestBody AuthRequest authRequest) {
         return successResponse(authService.login(authRequest), SystemMessage.USER_LOGGED_IN_MSG);
