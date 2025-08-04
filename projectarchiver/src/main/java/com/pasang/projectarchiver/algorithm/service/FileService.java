6af2941362bc6dc9fb83434156f12bb38cb54910
Pasang60 <pasanggelbu@gmail.com>
2025-08-04 23:02:25 +0545

    Unnecessary code block remove

--- projectarchiver/src/main/java/com/pasang/projectarchiver/algorithm/service/FileService.java
+++ projectarchiver/src/main/java/com/pasang/projectarchiver/algorithm/service/FileService.java
@@ -1,35 +1,3 @@
-//package com.pasang.projectarchiver.algorithm.service;
-//
-//import com.pasang.projectarchiver.algorithm.dto.CompressResponse;
-//import com.pasang.projectarchiver.algorithm.dto.FileRequest;
-//import com.pasang.projectarchiver.algorithm.dto.FileResponse;
-//import org.springframework.core.io.ByteArrayResource;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.http.ResponseEntity;
-//
-//import java.util.List;
-//
-//public interface FileService {
-//    FileResponse compressAndSaveFile(FileRequest fileRequest);
-//
-//    ResponseEntity<ByteArrayResource> downloadDecompressedFile(Long fileId);
-//
-//    Long getArchivedFilesCount();
-//
-//    Long getUserArchivedFilesCount();
-//
-//    List<CompressResponse> getAllArchivedFiles();
-//
-//    List<CompressResponse> getUserArchivedFiles();
-//}
-
-
-
-
-
-
-
 package com.pasang.projectarchiver.algorithm.service;
 
 import com.pasang.projectarchiver.algorithm.dto.CompressResponse;
@@ -37,9 +5,7 @@ import com.pasang.projectarchiver.algorithm.dto.FileRequest;
 import com.pasang.projectarchiver.algorithm.dto.FileResponse;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface FileService {
